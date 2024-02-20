8be538b4a6fd748f66c585235e6dbe682ed9b47e
Linus Dittmer <ldittmer@compchem05.iwr.uni-heidelberg.de>
2024-02-20 18:09:23 +0100

    Added small funcs in external parser

--- molbench/external_parser.py
+++ molbench/external_parser.py
@@ -1,14 +1,17 @@
 """Python file for all external parsers.
 
 """
-
+import os.path
 import molbench.logger as log
 
 class ExternalParser:
     """
     Parent class for an Output-API.
 
-    This class is used to load external data from a directory. Any class that is supposed to load external data from a directory must inherit from this class. The naming convention for subclasses is [SUITE]_[METHOD]_Parser (e. g. PySCF_FCI_Parser, QChem_MP2_Parser, ORCA_CCSDT_Parser).
+    This class is used to load external data from a directory. Any class that 
+    is supposed to load external data from a directory must inherit from this 
+    class. The naming convention for subclasses is [SUITE]_[METHOD]_Parser 
+    (e. g. PySCF_FCI_Parser, QChem_MP2_Parser, ORCA_CCSDT_Parser).
 
     Methods
     -------
@@ -23,8 +26,40 @@ class ExternalParser:
     def __init__(self):
         pass
 
-    def load(filepath: str, suffix: str = 'out') -> dict:
+    def load(self, filepath: str, suffix: str = 'out') -> dict:
         return None
+
+    def _fetch_all_outfiles(self, path: str, suffix: str = 'out') -> list:
+        outfiles = []
+
+        for root, _, files in os.walk(path):
+            for f in files:
+                if f.endswith(suffix):
+                    fp = os.path.abspath(os.path.join(root, f))
+                    outfiles.append(fp)
+
+        return outfiles
+
+    def _assign_outfiles_to_benchmark(self, outfiles: list, 
+                                      comp_benchmark: dict) -> dict:
+        assignment = {}
+        for outfile in outfiles:
+            for compkey in comp_benchmark:
+                if compkey[0] in outfile:
+                    assignment[outfile] = compkey
+                    break
+            if not outfile in assignment:
+                # TODO look into file to find assignment or go home and cry
+                pass
+        return assignment
+
+class QChem_MP2_Parser(ExternalParser):
+    
+    def __init__(self):
+        super().__init__()
+
+    def load(self, filepath: str, suffix: str = 'out') -> dict:
+        outfiles = self._fetch_all_outfiles(filepath, suffix)
 
 
 class PySCF_FCI_Parser(ExternalParser):
@@ -32,7 +67,7 @@ class PySCF_FCI_Parser(ExternalParser):
     def __init__(self):
         super().__init__()
 
-    def load(filepath: str, suffix: str = 'out') -> dict:
+    def load(self, filepath: str, suffix: str = 'out') -> dict:
         # TODO
         return None
 
